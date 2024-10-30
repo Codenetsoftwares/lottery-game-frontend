a0e6f568e442b443597f29db80ff5c7bc0b231ae
Codenetsoftwares <123172062+Codenetsoftwares@users.noreply.github.com>
2024-10-30 13:22:02 +0530

    Update UrlConstant.js

--- src/Utils/UrlConstant.js
+++ src/Utils/UrlConstant.js
@@ -1,8 +1,8 @@
 class UrlConstant {
   constructor() {}
 
-  //  url_dev = "https://lottery.server.dummydoma.in/api";
-  url_dev = 'http://localhost:8080/api';
+   url_dev = "https://lottery.server.dummydoma.in/api";
+  // url_dev = 'http://localhost:8080/api';
 
   // admin api
 
